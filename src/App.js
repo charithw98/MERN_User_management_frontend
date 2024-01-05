06752eed125714af098cfcc0ab1f6aca120e3efc
Your Name <charithweerasinghe27@gmail.com>
2024-01-05 15:22:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useNavigate } from "react-router-dom";
+import "./App.css"
 
 function App() {
-  return (
+
+  const navigate = useNavigate();
+
+  return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>User Management System</h1>
+        <button className="user_button" onClick={()=> navigate('/users')}>Users</button>
+
       </header>
     </div>
   );
 }
+
+
 
 export default App;
